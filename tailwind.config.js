2c38688f79a47d173eee79a2417affec54ac928b
bradybellini <bbellini@pm.me>
2020-12-02 01:33:30 -0800

    moved

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'body': ['Pokemon Pixel Font Regulars'],
+     },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
